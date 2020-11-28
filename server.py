02a30425e823bf148b1a90323564e288861fa63b
mkyurw <michaelyu103@gmail.com>
2020-11-28 19:34:49 +0800

    add english version

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index_en.html")
 
 @app.route("/<string:page_name>")
 def html_page(page_name):
